730f449902da7c1570101ef3a4f89431201ca21c
bobsmoke05 <bobhyvakka@gmail.com>
2026-02-23 14:56:06 +0200

    Register User Routes in app.js

--- starter/app.js
+++ starter/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const bookRouter = require("./routes/bookRouter");
+const userRouter = require("./routes/userRouter");
 const { unknownEndpoint, errorHandler } = require("./middleware/customMiddleware");
 const connectDB = require("./config/db");
 const cors = require("cors");
@@ -14,6 +15,8 @@ connectDB();
 
 // Use the bookRouter for all "/books" routes
 app.use("/api/books", bookRouter);
+// Use the userRouter for all "/users" routes
+app.use("/api/users", userRouter);
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
